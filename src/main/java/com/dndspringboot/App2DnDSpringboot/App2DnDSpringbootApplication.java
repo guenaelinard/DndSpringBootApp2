236b7b9bf445ece3ef0e536cd6b175fc0d0a5530
Sigmeeh <guenael.inard@le-campus-numerique.fr>
2023-11-27 15:24:38 +0100

    app2 SpringBoot Init

---
+++ src/main/java/com/dndspringboot/App2DnDSpringboot/App2DnDSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.dndspringboot.App2DnDSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App2DnDSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App2DnDSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dndspringboot/App2DnDSpringboot/App2DnDSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dndspringboot.App2DnDSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App2DnDSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
